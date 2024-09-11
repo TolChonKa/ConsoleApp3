440bce7ab7f1d24e7d01484c3873ae9ed01e6e59
TolChonKa <gekysik123@gmail.com>
2024-09-11 12:43:22 +0300

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.Write("Введите кол-во денег: ");
+decimal sum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во месяцев: ");
+int month = Convert.ToInt32(Console.ReadLine());
+decimal result = sum * Convert.ToDecimal (0.07);
+decimal global_result;
+for (int i = 0; i!= month  ; i++ )
+{
+    
+    global_result = sum + result;
+    sum = global_result;
+
+
+}
+Console.WriteLine($"Ваша сумма по вкладу составит: {global_result}");
+Console.ReadLine();
